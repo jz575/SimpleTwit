ae685de6352be6a0f77d56b6ed381b04a624e19e
Jeffrey Zhang <jz575@njit.edu>
2022-10-03 19:27:38 -0400

    5th video progress

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
@@ -3,13 +3,21 @@ package com.codepath.apps.restclienttemplate
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.codepath.apps.restclienttemplate.models.Tweet
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
+import org.json.JSONException
 
 private val TAG = "TimelineActivity"
 class TimelineActivity : AppCompatActivity() {
 
     lateinit var client: TwitterClient
+    lateinit var rvTweets: RecyclerView
+    lateinit var adapter: TweetsAdapter
+
+    val tweets = ArrayList<Tweet>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +27,11 @@ class TimelineActivity : AppCompatActivity() {
         supportActionBar?.setDisplayUseLogoEnabled(true)
 
         client = TwitterApplication.getRestClient(this)
+        rvTweets = findViewById(R.id.rvTweets)
+        adapter = TweetsAdapter(tweets)
+        rvTweets.layoutManager = LinearLayoutManager(this)
+        rvTweets.adapter = adapter
+
         populateHomeTimeline()
 
     }
@@ -26,8 +39,18 @@ class TimelineActivity : AppCompatActivity() {
     fun populateHomeTimeline() {
         client.getHomeTimeline(object: JsonHttpResponseHandler() {
 
-            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON?) {
-                Log.i(TAG,"API call successful $json")
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
+                Log.i(TAG,"API call successful")
+
+                val jsonArray = json.jsonArray
+
+                try {
+                    val newTweets = Tweet.fromJsonArray(jsonArray)
+                    tweets.addAll(newTweets)
+                    adapter.notifyDataSetChanged()
+                } catch (e:JSONException){
+                    Log.e(TAG,"JSON Exception $e")
+                }
             }
 
             override fun onFailure(
